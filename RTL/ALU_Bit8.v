09c38451b90ced8d5c2e2aefcdb41498095e18de
yogeshDE25 <yogeshs251103@gmail.com>
2026-07-24 18:51:37 +0530

    style: reformat Verilog source files

--- RTL/ALU_Bit8.v
+++ RTL/ALU_Bit8.v
@@ -6,32 +6,32 @@ module ALU_Bit8(
     output reg cout // in addition it will act as carry, in subtraction it will act as borrow
     );
     
-    always@* //combinational procedural statement
+    always @* //combinational procedural statement
     begin
     
     out = 16'b0;
     cout = 1'b0;
     
-    case(opcode)
+    case (opcode)
     
-    3'b000:{cout, out[7:0]} = a + b;// addition 
+    3'b000 : {cout, out[7:0]} = a + b;// addition 
     
-    3'b001:begin //subtraction
-        out = {{8{1'b0}}, a} - {{8{1'b0}}, b};
+    3'b001 : begin //subtraction
+        out = { {8 {1'b0} }, a} - { {8 {1'b0} }, b};
         cout = a < b;
         end
         
-    3'b010:out = a * b; //multiplication
+    3'b010 : out = a * b; //multiplication
     
-    3'b011:out = {{8{1'b0}}, a} << b[3:0];//left shift
+    3'b011 : out = {{8{1'b0}}, a} << b[3:0];//left shift
                 
-    3'b100:out= {{8{1'b0}}, a} >> b[3:0];//right shift
+    3'b100 : out = {{8{1'b0}}, a} >> b[3:0];//right shift
       
-    3'b101:out = a & b;//and
+    3'b101 : out = a & b;//and
                        
-    3'b110:out = a | b;//or
+    3'b110 : out = a | b;//or
                                  
-    3'b111:out= a ^ b;//xor
+    3'b111 : out = a ^ b;//xor
 
     default : ;
                                     
